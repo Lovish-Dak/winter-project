657f25c559237c1528ad873934017ecf137e9500
sharmaishaan21 <78147592+sharmaishaan21@users.noreply.github.com>
2022-01-15 00:12:13 +0530

    Save

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Containers/Home";
 import Checkout from "./Containers/Checkout";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
 
@@ -15,7 +16,7 @@ const App = () => {
         <Routes>
           <Route exact path="/checkout" element={<div><Header /><Checkout /></div>}>
           </Route>
-          <Route exact path="/" element={<div><Header /><Home /></div>}>
+          <Route exact path="/" element={<div><Header /><Home /><Footer /></div>}>
           </Route>
         </Routes>
       </div>
